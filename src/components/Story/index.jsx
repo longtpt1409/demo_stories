edc3728f87faaa2590aa59e78f3af0c4b1dcdea8
longtpt1409 <154228318+longtpt1409@users.noreply.github.com>
2024-04-17 14:08:27 +0700

    w

--- src/components/Story/index.jsx
+++ src/components/Story/index.jsx
@@ -18,7 +18,12 @@ export default function Story() {
   }, [param]);
   const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp);
-    return date.toLocaleString();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const formattedDay = day < 10 ? '0' + day : day;
+    const formattedMonth = month < 10 ? '0' + month : month;
+    return `${formattedDay}/${formattedMonth}/${year}`;
   };
   return (
     <div className="site-content">
