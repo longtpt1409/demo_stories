3257841f0287b4f9888b17ca8463f4ffdfcf4580
longtpt1409 <154228318+longtpt1409@users.noreply.github.com>
2024-04-17 14:19:44 +0700

    w

--- src/components/Story/index.jsx
+++ src/components/Story/index.jsx
@@ -21,9 +21,17 @@ export default function Story() {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
     const formattedDay = day < 10 ? '0' + day : day;
     const formattedMonth = month < 10 ? '0' + month : month;
-    return `${formattedDay}/${formattedMonth}/${year}`;
+    const formattedHours = hours < 10 ? '0' + hours : hours;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${formattedDay}/${formattedMonth}/${year}`;
   };
   return (
     <div className="site-content">
